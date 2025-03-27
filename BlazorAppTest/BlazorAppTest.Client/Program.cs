20e56035fb8cc353410c2e1eecb5497b6c82f4f9
CristianEstMaida <55913935+CristianEstMaida@users.noreply.github.com>
2025-03-27 10:16:33 -0300

    Add files via upload

---
+++ BlazorAppTest/BlazorAppTest.Client/Program.cs
@@ -0,0 +1,18 @@
+using BlazorAppTest.Client.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BlazorAppTest.Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton<TokenProviderService>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}

56f7b0fc161e94b3e57cdd5395eff633dfe5f83f
CristianEstMaida <55913935+CristianEstMaida@users.noreply.github.com>
2025-03-28 13:34:41 -0300

    Update WeatherForecastController.cs

--- BlazorAppTest/BlazorAppTest/Controllers/WeatherForecastController.cs
+++ BlazorAppTest/BlazorAppTest/Controllers/WeatherForecastController.cs
@@ -26,13 +26,27 @@ namespace BlazorAppTest.Controllers
 
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index =>
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                int temperatureC = Random.Shared.Next(-20, 55);
+            
+                return new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = temperatureC,
+                    Summary = Summaries[
+                        (temperatureC >= 40) ? 9 :
+                        (temperatureC >= 35) ? 8 :
+                        (temperatureC >= 30) ? 7 :
+                        (temperatureC >= 27) ? 6 :
+                        (temperatureC >= 25) ? 5 :
+                        (temperatureC >= 18) ? 4 :
+                        (temperatureC >= 10) ? 3 :
+                        (temperatureC >= 0) ? 2 :
+                        (temperatureC >= -10) ? 1 : 0
+                    ]
+                };
+            }).ToArray();
         }
     }
 }
